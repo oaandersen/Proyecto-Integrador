3b4152abd167057b632e6e3fb3957c2953ad16b3
joacobrandan <jbrandanquarto@udesa.edu.ar>
2021-06-22 23:08:37 -0300

    correcion for playlist

--- js/playlist.js
+++ js/playlist.js
@@ -1,28 +1,40 @@
 window.addEventListener('load',function(){
     
     let proxy = 'https://cors-anywhere.herokuapp.com/';
+    let api=  "https://api.deezer.com/track/";
     
     let recuperoStorage = localStorage.getItem('playlist');
+    
     let playlist = JSON.parse(recuperoStorage);
+    
 
     console.log(playlist);
+    
 
     let listaTracks = document.querySelector(".medioplaylist");
+    
     if( playlist.length == 0 ) {
         listaTracks.innerHTML += ` 
         <p>Aun no hay canciones en tu playlist</p>`
     
-    } else{
-        playlist.forEach(function(idTrack) {
-            encontrarTrack(idTrack)
-        });
+    } else{ 
+            for ( let i=0; i<playlist.length; i++ ) {
+                encontrarTrack( playlist[i] );
+            }
+        
     }
+    
 function encontrarTrack(idTrack){
-    let url = proxy + "https://api.deezer.com/track/" + idTrack;
+    
+    let url = proxy + api + idTrack;
+
+    
     fetch(url)
     .then(function(response){
      return response.json();
+     
     })
+    
     .then (function(tracks){
     console.log(tracks)
     let untrack = tracks;
@@ -33,6 +45,7 @@ function encontrarTrack(idTrack){
         <h3> <a class="decoration" href="detail-track.html?id=${iduntrack}">${nombretrack} </a> </h3>
     </article>
     ` 
+    
     })
     .catch(function(error){
         console.log(error)
